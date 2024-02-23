0f6a47d4717042b00cc8449fde6a8415d9c08681
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-02-23 23:45:29 +0700

    add one more section

--- app/page.jsx
+++ app/page.jsx
@@ -154,6 +154,93 @@ export default function LandingPage() {
         </div>
       </div>
       {/*  */}
+      <div className='flex flex-col text-center px-[11.25rem] text-white py-20'>
+        <div className='pb-[0.63rem]'>
+          <h1 className='font-raleway text-[2.125rem] font-extrabold'>
+            Testimonials
+          </h1>
+        </div>
+        <div className='pb-[3.75rem] px-44'>
+          <p className='text-[#9C9C9C]'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className='grid grid-cols-2 text-left'>
+          <div className='flex flex-col gap-8 p-8'>
+            <div>
+              <p className='text-[#9C9C9C]'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
+            </div>
+            <div className='flex flex-row items-center gap-4'>
+              <div>
+                <img src='/images/cp-1.png' alt='' />
+              </div>
+              <div>
+                <h2 className='text-lg font-semibold font-raleway'>
+                  Client Name
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-8 p-8'>
+            <div>
+              <p className='text-[#9C9C9C]'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
+            </div>
+            <div className='flex flex-row items-center gap-4'>
+              <div>
+                <img src='/images/cp-2.png' alt='' />
+              </div>
+              <div>
+                <h2 className='text-lg font-semibold font-raleway'>
+                  Client Name
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-8 p-8'>
+            <div>
+              <p className='text-[#9C9C9C]'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
+            </div>
+            <div className='flex flex-row items-center gap-4'>
+              <div>
+                <img src='/images/cp-3.png' alt='' />
+              </div>
+              <div>
+                <h2 className='text-lg font-semibold font-raleway'>
+                  Client Name
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-8 p-8'>
+            <div>
+              <p className='text-[#9C9C9C]'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
+            </div>
+            <div className='flex flex-row items-center gap-4'>
+              <div>
+                <img src='/images/cp-4.png' alt='' />
+              </div>
+              <div>
+                <h2 className='text-lg font-semibold font-raleway'>
+                  Client Name
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
